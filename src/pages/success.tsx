0cbb54068c41c459fb3a30bb403959f1813885be
MarcosvBueno <mvbueno07@gmail.com>
2023-11-09 14:35:38 -0300

    style(successPage): adding structure and style

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,6 +1,20 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 function Success() {
   return ( <>
-    <h1>Success Page</h1>
+    <SuccessContainer>
+      <h1>Compra Efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+      <p>Uhuul <strong>Diego Fernandes</strong>, sua <strong>compra de 3 camisetas</strong> já está a caminho da sua casa. </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   </> );
 }
 
